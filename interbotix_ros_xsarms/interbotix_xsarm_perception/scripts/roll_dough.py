251a5ebf1d4c699988889a2feebd7cdcb7c8293b
Janko <jo951030@gmail.com>
2022-04-28 22:54:21 -0400

    Add skeleton for the main code

--- interbotix_ros_xsarms/interbotix_xsarm_perception/scripts/roll_dough.py
+++ interbotix_ros_xsarms/interbotix_xsarm_perception/scripts/roll_dough.py
@@ -1,9 +1,17 @@
-import time
+######################################################################################################
+# Robotic Dough Shaping - Main file
+######################################################################################################
+# Instructions:
+#   First, run: roslaunch interbotix_xsarm_perception xsarm_perception.launch robot_model:=wx250s
+#   Then change to this directory and run: python3 roll_dough.py
+######################################################################################################
+
+from time import time
+import math.pi as PI
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
 from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterface
 
-# First, run: roslaunch interbotix_xsarm_perception xsarm_perception.launch robot_model:=wx250s
-# Then change to this directory and type: python3 roll_dough.py
+
 
 
 def main():
@@ -11,80 +19,97 @@ def main():
     bot = InterbotixManipulatorXS("wx250s")#, moving_time=5, accel_time=5)
     pcl = InterbotixPointCloudInterface()
 
-    # Move to ReadyPose
-    bot.arm.set_ee_pose_components(x=?, y=0, z=?, pitch=1.57)
-    # Record the target dough shape
-    todo
+    #########################
+    # Settings
+    #########################
 
+    method = 'basic'
+    # Choose one termination condition
+    termination_condition = 'max_time'
+    termination_condition = 'min_iou'
+    max_time = 10 # in seconds
+    min_iou = 0.5
+
+    if termination_condition == 'max_time':
+        def terminate(t, iou):
+            return t >= max_time
+    elif termination_condition == 'min_iou':
+        def terminate(t, iou):
+            return iou >= min_iou
+    else:
+        raise ValueError(f'Unknown terminationcondition {termination_condition}')
+
+    # Logging
+    print('=' * 120)
+    print(f'Method: {method}')
+    print(f'Termination condition: {termination_condition}')
+    if termination_condition == 'max_time':
+        print(f'\t when time >= {max_time} s')
+    elif termination_condition == 'min_iou':
+        print(f'\t when  IoU >= {min_iou}')
+    print('=' * 120)
+
+    #########################
+    # Preparation
+    #########################
+    start_time = time()
+    iou = 0.
+
+    # Move to ReadyPose
+    bot.arm.set_ee_pose_components(x=0.0567, y=0, z=0.15812, pitch=PI/2)
+
+    # Record the target dough shape
+    #TODO
+
+    # Calculate current IoU
+    #TODO
+    # iou = calculate_iou()
+
+    # Logging
+    print(f'Preparation time: {time() - start_time} s')
+    print('=' * 120)
+
+    #########################
     # Iterative procedure
-    while not terminate:
+    #########################
+    iteration = 1
+    while not terminate(time() - start_time, iou):
 
         # Detect the roll starting point S
+        #TODO
         _, cluster = pcl.get_cluster_positions(ref_frame="wx250s/base_link", sort_axis="x", reverse=True)
         S = cluster['position']
+        print(f'Detected roll start point S: x, y, z =')
 
         # Calculate the roll goal point G
-        todo
+        #TODO
+        print(f'Calculated roll goal point G: x, y, z =')
 
         # Move to AboveBeforePose
-        bot.arm.set_ee_pose_components(x=Sx, y=Sy, z=Sz+d, pitch=1.57, yaw=angle(S->G))
+        bot.arm.set_ee_pose_components(x=Sx, y=Sy, z=Sz+d, pitch=PI/2, yaw=angle(S->G))
 
         # Move to TouchPose
-        bot.arm.set_ee_pose_components(x=Sx, y=Sy, z=Sz, pitch=1.57, yaw=angle(S->G))
+        bot.arm.set_ee_pose_components(x=Sx, y=Sy, z=Sz, pitch=PI/2, yaw=angle(S->G))
 
         # Perform roll to point G
-        bot.arm.set_ee_pose_components(x=Gx, y=Gy, z=Gz, pitch=1.57, yaw=angle(S->G))
+        bot.arm.set_ee_pose_components(x=Gx, y=Gy, z=Gz, pitch=PI/2, yaw=angle(S->G))
 
         # Move to AboveAfterPose
-        bot.arm.set_ee_pose_components(x=Gx, y=Gy, z=Gz+d, pitch=1.57, yaw=angle(S->G))
+        bot.arm.set_ee_pose_components(x=Gx, y=Gy, z=Gz+d, pitch=PI/2, yaw=angle(S->G))
 
         # Move to ReadyPose
-        bot.arm.set_ee_pose_components(x=?, y=0, z=?, pitch=1.57)
+        bot.arm.set_ee_pose_components(x=0.0567, y=0, z=0.15812, pitch=PI/2)
 
+        # Calculate current IoU
+        #TODO
+        # iou = calculate_iou()
 
+        # Logging
+        print(f'Iteration: {iteration:7d}\t Time: {time() - start_time:5.3f} s\t IoU: {iou:.3f}')
+        print('=' * 120)
 
+        iteration += 1
 
-
-
-    # set initial arm and gripper pose
-    # bot.arm.set_ee_pose_components(x=0.3, z=0.2)
-    # bot.gripper.open()
-
-    # get the ArmTag pose
-    # bot.arm.set_ee_pose_components(y=-0.3, z=0.2)
-    # time.sleep(0.5)
-    # armtag.find_ref_to_arm_base_transform()
-    bot.arm.go_to_sleep_pose()
-    # bot.arm.go_to_home_pose()
-    # bot.arm.set_ee_cartesian_trajectory(x=-0.4, z=-0.2, pitch=1.57)
-    bot.arm.set_ee_pose_components(x=0.0567, z=0.15812, pitch=1.57)
-    # bot.arm.set_ee_pose_components(pitch=1.57)
-    # bot.arm.go_to_sleep_pose()
-
-    # get the cluster positions
-    # sort them from max to min 'x' position w.r.t. the 'wx200/base_link' frame
-    success, clusters = pcl.get_cluster_positions(ref_frame="wx250s/base_link", sort_axis="x", reverse=True)
-    print("-------number of clusters:  ", len(clusters), clusters)
-
-    # pick up all the objects and drop them in a virtual basket in front of the robot
-    # for cluster in clusters:
-    #     x, y, z = cluster["position"]
-    #     print(x,y,z)
-    #     # for i in range(10):
-    #     # bot.arm.set_ee_pose_components(x=x-0.03, y=y, z=z+0.1, pitch=1.57)
-    #     # bot.arm.set_ee_pose_components(x=x-0.03, y=y, z=z+0.05, pitch=1.57)
-    #     bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.1, pitch=1.57)
-    #     bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.05, pitch=1.57)
-    #     # bot.gripper.close()
-    #     bot.arm.set_ee_pose_components(x=x+0.02, y=y, z=z+0.05, pitch=1.57)
-
-    #     # --------------------
-
-    #     bot.arm.set_ee_pose_components(x=x, y=y, z=z+0.1, pitch=1.57, yaw=1.57)
-    #     bot.arm.set_ee_pose_components(x=x, y=y-0.02, z=z+0.1, pitch=1.57, yaw=1.57)
-    #     # bot.arm.set_ee_pose_components(x=0.3, z=0.2, pitch=1.57)
-    #     # bot.gripper.open()
-    # bot.arm.go_to_sleep_pose()
 
 if __name__=='__main__':
     main()
