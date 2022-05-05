4d4ee82188643b947e88fa840f2bfb9fd76fd4d4
Jan (Janko) Ondras <jo951030@gmail.com>
2022-05-05 17:26:06 -0400

    Log to csv during execution

--- interbotix_ros_xsarms/interbotix_xsarm_perception/scripts/roll_dough.py
+++ interbotix_ros_xsarms/interbotix_xsarm_perception/scripts/roll_dough.py
@@ -6,8 +6,13 @@
 #   Then change to this directory and run: python3 roll_dough.py
 ######################################################################################################
 
-from time import time
+import json
+import os
+import csv
+import argparse
 import numpy as np
+from time import time
+from datetime import datetime
 from interbotix_xs_modules.arm import InterbotixManipulatorXS
 from interbotix_perception_modules.pointcloud import InterbotixPointCloudInterface
 
@@ -18,107 +23,124 @@ def calculate_iou():
 
 
 def main():
-    # Initialize the arm module along with the pointcloud module
+    #########################
+    # Parse args
+    #########################
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--method', type=str, default='basic', help='Choose the method from: "basic", ...')
+    parser.add_argument('-tc', '--termination-condition', type=str, default='time', help='Choose either "time" or "iou" termination condition.')
+    parser.add_argument('-tv', '--termination-value', type=float, default=10., help='Either maximum time in seconds or minimum IoU based on the termination-condition argument.')
+    parser.add_argument('-ld', '--log-dir', type=str, default='./logs', help='Path to directory where to save logs.') 
+    parser.add_argument('-za', '--z-above', type=float, default=0.15812, help='Vertical distance above the dough immediately before and after rolling.')    
+    args = parser.parse_args()
+
+    if args.termination_condition == 'time':
+        def terminate(t, iou):
+            return t >= args.termination_value
+    elif args.termination_condition == 'iou':
+        def terminate(t, iou):
+            return iou >= args.termination_value
+    else:
+        raise ValueError(f'Unknown termination condition {args.termination_condition}')
+
+    #########################
+    # Initialize robot
+    #########################
+
     bot = InterbotixManipulatorXS("wx250s")#, moving_time=5, accel_time=5)
     pcl = InterbotixPointCloudInterface()
 
     #########################
-    # Settings
+    # Setup logging
     #########################
 
-    method = 'basic'
-    # Choose one of the termination conditions below
-    termination_condition = 'max_time'
-    termination_condition = 'min_iou'
-    max_time = 10 # in seconds
-    min_iou = 0.5
-    # Vertical distance above the dough immediately before and after rolling
-    z_above = 0.15812
-
-    if termination_condition == 'max_time':
-        def terminate(t, iou):
-            return t >= max_time
-    elif termination_condition == 'min_iou':
-        def terminate(t, iou):
-            return iou >= min_iou
-    else:
-        raise ValueError(f'Unknown termination condition {termination_condition}')
-
-    # Logging
     print('=' * 120)
-    print(f'Method: {method}')
-    print(f'Termination condition: {termination_condition}')
-    if termination_condition == 'max_time':
-        print(f'\t when time >= {max_time} s')
-    elif termination_condition == 'min_iou':
-        print(f'\t when  IoU >= {min_iou}')
+    print(json.dumps(vars(args), indent=2))
     print('=' * 120)
 
-    #########################
-    # Preparation
-    #########################
-    start_time = time()
-    iou = 0.
+    if not os.path.isdir(args.log_dir):
+        os.makedirs(args.log_dir)
 
-    # Move to ReadyPose
-    bot.arm.set_ee_pose_components(x=0.0567, y=0, z=0.15812, pitch=np.pi/2)
+    log_filename = f'log_{args.termination_condition}_{args.termination_value}_{args.method}_{datetime.now().strftime("%Y%m%d-%H%M%S-%f")}'
+    with open(f'{args.log_dir}/{log_filename}.csv', 'w', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
 
-    # Record the target dough shape
-    #TODO
+        # First line is a dictionary of parameters
+        csv_writer.writerow([json.dumps(vars(args))])
+        # Second line is the header
+        csv_writer.writerow(['Iteration', 'Time', 'IoU'])
 
-    # Calculate current IoU
-    #TODO
-    # iou = calculate_iou()
-
-    # Logging
-    print(f'Preparation: \t Time: {time() - start_time:5.3f} s\t IoU: {iou:.3f}')
-    print('=' * 120)
-
-    #########################
-    # Iterative procedure
-    #########################
-    iteration = 1
-    while not terminate(time() - start_time, iou):
-
-        # Detect the roll starting point S
-        #TODO
-        _, cluster = pcl.get_cluster_positions(ref_frame="wx250s/base_link", sort_axis="x", reverse=True)
-        S = cluster['position']
-        print(f'Detected roll start point S: x, y, z =')
-
-        # Calculate the roll goal point G
-        #TODO
-        print(f'Calculated roll goal point G: x, y, z =')
-
-        # Calculate the angle of the direction S -> G
-        # No need to use arctan2 due to symmetry
-        yaw_SG = np.arctan((Gy - Sy) / (Gx - Sx))
-        print(f'Calculated the angle of the direction S -> G: {yaw_SG * 180 / np.pi}')
-
-        # Move to AboveBeforePose
-        bot.arm.set_ee_pose_components(x=Sx, y=Sy, z=Sz + z_above, pitch=np.pi/2, yaw=yaw_SG)
-
-        # Move to TouchPose
-        bot.arm.set_ee_pose_components(x=Sx, y=Sy, z=Sz, pitch=np.pi/2, yaw=yaw_SG)
-
-        # Perform roll to point G
-        bot.arm.set_ee_pose_components(x=Gx, y=Gy, z=Gz, pitch=np.pi/2, yaw=yaw_SG)
-
-        # Move to AboveAfterPose
-        bot.arm.set_ee_pose_components(x=Gx, y=Gy, z=Gz + z_above, pitch=np.pi/2, yaw=yaw_SG)
+        #########################
+        # Preparation
+        #########################
+        iteration = 0
+        start_time = time()
+        iou = 0.
 
         # Move to ReadyPose
         bot.arm.set_ee_pose_components(x=0.0567, y=0, z=0.15812, pitch=np.pi/2)
+
+        # Record the target dough shape
+        #TODO
 
         # Calculate current IoU
         #TODO
         # iou = calculate_iou()
 
         # Logging
-        print(f'Iteration: {iteration:7d}\t Time: {time() - start_time:5.3f} s\t IoU: {iou:.3f}')
+        time_elapsed = time() - start_time
+        print(f'Preparation: \t Time: {time_elapsed:5.3f} s\t IoU: {iou:.3f}')
         print('=' * 120)
+        csv_writer.writerow([iteration, time_elapsed, iou])
 
-        iteration += 1
+        #########################
+        # Iterative procedure
+        #########################
+        iteration = 1
+        while not terminate(time() - start_time, iou):
+
+            # Detect the roll starting point S
+            #TODO
+            _, cluster = pcl.get_cluster_positions(ref_frame="wx250s/base_link", sort_axis="x", reverse=True)
+            S = cluster['position']
+            print(f'Detected roll start point S: x, y, z =')
+
+            # Calculate the roll goal point G
+            #TODO
+            print(f'Calculated roll goal point G: x, y, z =')
+
+            # Calculate the angle of the direction S -> G
+            # No need to use arctan2 due to symmetry
+            yaw_SG = np.arctan((Gy - Sy) / (Gx - Sx))
+            print(f'Calculated the angle of the direction S -> G: {yaw_SG * 180 / np.pi}')
+
+            # Move to AboveBeforePose
+            bot.arm.set_ee_pose_components(x=Sx, y=Sy, z=Sz + args.z_above, pitch=np.pi/2, yaw=yaw_SG)
+
+            # Move to TouchPose
+            bot.arm.set_ee_pose_components(x=Sx, y=Sy, z=Sz, pitch=np.pi/2, yaw=yaw_SG)
+
+            # Perform roll to point G
+            bot.arm.set_ee_pose_components(x=Gx, y=Gy, z=Gz, pitch=np.pi/2, yaw=yaw_SG)
+
+            # Move to AboveAfterPose
+            bot.arm.set_ee_pose_components(x=Gx, y=Gy, z=Gz + args.z_above, pitch=np.pi/2, yaw=yaw_SG)
+
+            # Move to ReadyPose
+            bot.arm.set_ee_pose_components(x=0.0567, y=0, z=0.15812, pitch=np.pi/2)
+
+            # Calculate current IoU
+            #TODO
+            # iou = calculate_iou()
+
+            # Logging
+            time_elapsed = time() - start_time
+            print(f'Iteration: {iteration:7d}\t Time: {time_elapsed:5.3f} s\t IoU: {iou:.3f}')
+            print('=' * 120)
+            csv_writer.writerow([iteration, time_elapsed, iou])
+
+            iteration += 1
 
 
 if __name__=='__main__':
